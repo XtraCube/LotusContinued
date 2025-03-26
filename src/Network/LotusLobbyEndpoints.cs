5d87b9a2b370e64d88c21e036329ed506c04e091
ThetaHalo <theta@eps.lol>
2025-03-26 03:16:15 -0500

    Update Lobby Endpoint

--- src/Network/LotusLobbyEndpoints.cs
+++ src/Network/LotusLobbyEndpoints.cs
@@ -11,8 +11,8 @@ public class LotusLobbyEndpoints : ILobbyServerInfo
     private static readonly Dictionary<string, string> Empty = new();
     private const bool IsDebug = false;
 
-    public string CreateEndpoint() => IsDebug ? "https://testing-lotus.eps.lol/lobbies" : "https://lobbies.lotusau.top/lobbies";
-    public string UpdatePlayerStatusEndpoint() => IsDebug ? "https://testing-lotus.eps.lol/update-lobby" : "https://lobbies.lotusau.top/update-lobby";
+    public string CreateEndpoint() => IsDebug ? $"https://testing-lotus.eps.lol/lobbies/{AmongUsClient.Instance.GameId}" : $"https://lobbies.lotusau.top/lobbies/{AmongUsClient.Instance.GameId}";
+    public string UpdatePlayerStatusEndpoint() => IsDebug ? $"https://testing-lotus.eps.lol/lobbies/{AmongUsClient.Instance.GameId}/update" : $"https://lobbies.lotusau.top/lobbies/{AmongUsClient.Instance.GameId}/update";
     public string UpdateMapEndpoint() => UpdatePlayerStatusEndpoint();
     public Dictionary<string, string> AddCustomHeaders(LobbyUpdateType _) => Empty;
 }
