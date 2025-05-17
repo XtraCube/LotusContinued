85a1c450bdf8e6c009e32e1be3f6792d1ee9ca95
Discussions <80537729+discus-sions@users.noreply.github.com>
2025-05-17 14:47:30 -0500

    Fix cooldown bug
    If finish was called before StartThenRun, it would skip the action.

--- src/GUI/Cooldown.cs
+++ src/GUI/Cooldown.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using Lotus.Logging;
 using Lotus.Managers;
 using Lotus.Roles.Internals.Interfaces;
 using Lotus.Roles.Attributes;
@@ -35,14 +36,14 @@ public class Cooldown : ICloneOnSetup<Cooldown>
     public bool IsReady() => TimeRemaining() <= 0;
     public void Start(float duration = float.MinValue)
     {
-        remaining = duration == float.MinValue ? Duration : duration;
+        remaining = duration < 0 ? Duration : duration;
         lastTick = DateTime.Now;
         if (IsCoroutine) CooldownManager.SubmitCooldown(this);
     }
 
     public void Finish(bool skipAction = false)
     {
-        skipNextCall = skipAction;
+        skipNextCall = skipAction && action != null;
         remaining = 0.01f;
     }
 
@@ -63,8 +64,8 @@ public class Cooldown : ICloneOnSetup<Cooldown>
         if (remaining > 0 || action == null) return remaining;
         Action tempAction = action;
         action = null;
-        if (!skipNextCall) tempAction();
-        skipNextCall = false;
+        if (skipNextCall) skipNextCall = false;
+        else tempAction();
         return remaining;
     }
 
