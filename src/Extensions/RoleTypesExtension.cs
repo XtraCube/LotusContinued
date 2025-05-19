623785bccfd03279ea01770eead2f340a6b0eb97
Discussions <80537729+discus-sions@users.noreply.github.com>
2025-05-18 19:30:59 -0500

    Fix Typo

--- src/Extensions/RoleTypesExtension.cs
+++ src/Extensions/RoleTypesExtension.cs
@@ -7,5 +7,5 @@ public static class RoleTypesExtension
     public static bool IsImpostor(this RoleTypes roleTypes) => roleTypes is RoleTypes.Impostor or RoleTypes.Shapeshifter or RoleTypes.ImpostorGhost or RoleTypes.Phantom;
     public static bool IsCrewmate(this RoleTypes roleTypes) => !IsImpostor(roleTypes);
 
-    public static RoleTypes GhostEquivelant(this RoleTypes roleTypes) => roleTypes.IsImpostor() ? RoleTypes.ImpostorGhost : RoleTypes.CrewmateGhost;
+    public static RoleTypes GhostEquivalent(this RoleTypes roleTypes) => roleTypes.IsImpostor() ? RoleTypes.ImpostorGhost : RoleTypes.CrewmateGhost;
 }
