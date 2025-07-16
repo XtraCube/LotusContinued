ad07533709a9d030a5ec9470296af202ffd4a5d2
ThetaHalo <theta@eps.lol>
2025-07-16 16:40:12 -0500

    add missing task 2

--- src/Patches/Systems/RpcSetTasksPatch.cs
+++ src/Patches/Systems/RpcSetTasksPatch.cs
@@ -181,6 +181,7 @@ public class RpcSetTasksPatch
             case TaskTypes.FuelEngines when GeneralOptions.GameplayOptions.DisabledLongTaskFlag.HasFlag(DisabledLongTask.FuelEngines):
             case TaskTypes.ExtractFuel when GeneralOptions.GameplayOptions.DisabledLongTaskFlag.HasFlag(DisabledLongTask.FuelEngines): // variation of FuelEngines?
             case TaskTypes.InspectSample when GeneralOptions.GameplayOptions.DisabledLongTaskFlag.HasFlag(DisabledLongTask.InspectSample):
+            case TaskTypes.OpenWaterways when GeneralOptions.GameplayOptions.DisabledLongTaskFlag.HasFlag(DisabledLongTask.OpenWaterways):
             case TaskTypes.RebootWifi when GeneralOptions.GameplayOptions.DisabledLongTaskFlag.HasFlag(DisabledLongTask.RebootWifi):
             case TaskTypes.ReplaceWaterJug when GeneralOptions.GameplayOptions.DisabledLongTaskFlag.HasFlag(DisabledLongTask.ReplaceWaterJug):
             case TaskTypes.ResetBreakers when GeneralOptions.GameplayOptions.DisabledLongTaskFlag.HasFlag(DisabledLongTask.ResetBreaker):
